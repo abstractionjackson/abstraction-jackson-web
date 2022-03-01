2769c43c898e6fddb34324acf72f50019466462b
Jackson Galan <47265694+abstractionjackson@users.noreply.github.com>
2022-02-28 16:34:13 -0800

    add Header

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let header;
+    export let subHeader;
+</script>
+
+<main>
+    <heading>
+        <h1>{header}</h1>
+        <h2>{subHeader}</h2>
+    </heading>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Header from '$lib/components/Header.svelte'
+    export let heading = {
+        header: 'Welcome',
+        subHeader: 'Feb 28th, 2022'
+    }
+</script>
+
+<main>
+    <header>
+        <Header {...heading} />
+    </header>
+</main>
