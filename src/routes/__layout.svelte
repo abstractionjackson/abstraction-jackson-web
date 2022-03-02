a5f7f2f2e785ecc50161326e01c4db7355fde4c0
Jackson Galan <47265694+abstractionjackson@users.noreply.github.com>
2022-03-01 16:37:43 -0800

    add layout component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,33 @@
 <script>
 	import '../app.css';
+	//biuold the nav links from page
+	export let links = [
+		{
+			href: '/',
+			label: 'Welcome'
+		},
+		{
+			href: '/projects',
+			label: 'projects'
+		},
+		{
+			href: '/blog',
+			label: 'blog'
+		},
+	]
 </script>
 
+<main>
+	<header>
+		<nav>
+			<ul>
+				{#each links as {href,label}}
+					<li>
+						<a href={href}>{label}</a>
+					</li>
+				{/each}
+			</ul>	
+		</nav>
+	</header>
+	</main>
 <slot />
